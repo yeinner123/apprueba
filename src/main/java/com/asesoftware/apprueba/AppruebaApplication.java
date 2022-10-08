1a7df857c98db70fcdd7db4e6f2951b3dede9561
Yeinner Zambrano <yeinner123@gmail.com>
2022-10-08 09:33:33 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/asesoftware/apprueba/AppruebaApplication.java
@@ -0,0 +1,13 @@
+package com.asesoftware.apprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asesoftware/apprueba/AppruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asesoftware.apprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
