a94be2b332146f7ad151228cd2f1266fb958c6fc
yeinner123 <yeinner123@gmail.com>
2022-10-20 00:47:12 -0500

    delete
    delete de todo... empezar desde 0

--- src/main/java/com/asesoftware/apprueba/dao/ServicioDao.java
+++
@@ -1,8 +0,0 @@
-package com.asesoftware.apprueba.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.asesoftware.apprueba.model.Servicio;
-
-public interface ServicioDao extends JpaRepository<Servicio, Integer>{
-
-}
--- src/main/java/com/asesoftware/apprueba/dao/TurnoDao.java
+++
@@ -1,8 +0,0 @@
-package com.asesoftware.apprueba.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.asesoftware.apprueba.model.Turno;
-
-public interface TurnoDao extends JpaRepository<Turno, Integer> {
-
-}
